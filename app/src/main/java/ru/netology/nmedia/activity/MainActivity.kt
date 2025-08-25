769b853d281f7da65862e2757066964fa161a7f6
Dennis Kostyuk <dk0110@yandex.ru>
2025-08-25 03:05:27 +0300

    initial commit

---
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -0,0 +1,37 @@
+package ru.netology.nmedia.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import ru.netology.nmedia.viewmodel.PostViewMode
+import ru.netology.nmedia.R
+import ru.netology.nmedia.adapter.PostAdapter
+import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.databinding.PostCardBinding
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val viewModel: PostViewMode by viewModels()
+
+        val adapter = PostAdapter { post ->
+            viewModel.like(post.id)
+            viewModel.repost(post.id)
+        }
+
+        binding.list.adapter = adapter
+
+        viewModel.data.observe(this) { posts ->
+            adapter.list = posts
+        }
+
+    }
+}
